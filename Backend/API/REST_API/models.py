aa24be85563d2e9727696a64087e6aee509dab67
jptboy <vegetablebat1@gmail.com>
2018-11-18 19:07:56 -0700

    models created

--- Backend/API/REST_API/models.py
+++ Backend/API/REST_API/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class UserAccount(models.Model):
+    username = models.CharField(max_length = 100)
+    password = models.CharField(max_length = 100)
+
+class Bike(models.Model):
+    name = models.CharField(max_length = 100)
+    locationLat = models.DecimalField(max_digits=20, decimal_places=4)
+    locationLong = models.DecimalField(max_digits=20, decimal_places=4)
+    state = models.CharField(max_length = 100)
+    owner = models.ForeignKey(UserAccount,on_delete=models.CASCADE)
