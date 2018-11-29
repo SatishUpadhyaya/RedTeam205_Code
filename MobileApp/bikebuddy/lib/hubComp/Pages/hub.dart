0f724789864eb5b41e9ae241e0ce8b9cea56d86a
hainsworth99 <31012794+hainsworth99@users.noreply.github.com>
2018-11-29 13:03:00 -0700

    Added AppBar to replace header.

--- MobileApp/bikebuddy/lib/hubComp/Pages/hub.dart
+++ MobileApp/bikebuddy/lib/hubComp/Pages/hub.dart
@@ -1,50 +1,6 @@
 import 'package:flutter/material.dart';
 
-Widget BikeProfile(String bikeName){
-	return new Container(
-		padding: const EdgeInsets.all(10.0),
-		child: new Card(
-			color: Colors.blue,
-			child: new Row(
-				children: <Widget> [
-					// bike icon
-					new Container(
-						padding: const EdgeInsets.all(10.0),
-						child: new IconButton(
-							icon: new Icon(Icons.directions_bike),
-						),
-					),
-					// bike name
-					new Container(
-						padding: const EdgeInsets.all(10.0),
-						child: new Text(
-							bikeName,
-							style: TextStyle(
-								fontWeight: FontWeight.bold,
-								fontSize: 14.0,
-								color: Colors.white,
-							)
-						),
-					),	
-					// lock button
-					new Container(
-						padding: const EdgeInsets.all(10.0),
-						child: new IconButton(
-							icon: new Icon(Icons.lock),
-						),
-										),
-					// delete button
-					new Container(
-						padding: const EdgeInsets.all(10.0),
-						child: new IconButton(
-							icon: new Icon(Icons.delete),
-						),
-					),
-				]
-			)
-		)
-	);
-}
+
 
 class HubPage extends StatefulWidget{
   @override
@@ -53,10 +9,70 @@ class HubPage extends StatefulWidget{
 
 class HomePageState extends State<HubPage>{
 
+	Widget BikeProfile(String bikeName){
+		return new Container(
+			padding: const EdgeInsets.all(10.0),
+			child: new Card(
+				color: Colors.blue,
+				child: new Row(
+					children: <Widget> [
+						// bike icon
+						new Container(
+							padding: const EdgeInsets.all(10.0),
+							child: new IconButton(
+								icon: new Icon(Icons.directions_bike),
+							),
+						),
+						// bike name
+						new Container(
+							padding: const EdgeInsets.all(10.0),
+							child: new Text(
+								bikeName,
+								style: TextStyle(
+									fontWeight: FontWeight.bold,
+									fontSize: 14.0,
+									color: Colors.white,
+								)
+							),
+						),	
+						// lock button
+						new Container(
+							padding: const EdgeInsets.all(10.0),
+							child: new IconButton(
+								icon: new Icon(Icons.lock),
+							),
+											),
+						// delete button
+						new Container(
+							padding: const EdgeInsets.all(10.0),
+							child: new IconButton(
+								icon: new Icon(Icons.delete),
+							),
+						),
+					]
+				)
+			)
+		);
+	}
+
 	@override 
 	Widget build(BuildContext context){
 		return new Scaffold(
-			backgroundColor: Colors.black,
+			appBar: AppBar(
+				title: new Text(
+					"Welcome, <firstname>!",
+					style: TextStyle(
+						fontWeight: FontWeight.bold,
+						fontSize: 30.0,
+						color: Colors.white,
+					)
+				),
+				actions: <Widget>[
+					new IconButton(
+						icon: Icon(Icons.person),
+					)
+				]
+			),
 			body: new Stack(
 				fit: StackFit.expand,
 				children: <Widget> [
@@ -74,31 +90,21 @@ class HomePageState extends State<HubPage>{
 					
 					new Column (
 						children: <Widget> [
-							// Welcome header
-							new Container(
-								width: 2050.0,
-								padding: const EdgeInsets.fromLTRB(10.0, 40.0, 10.0, 10.0),
-								child: new Card(
-									color: Colors.blue,
-									child: new Container(
-										padding: const EdgeInsets.all(10.0),
-										child: new Center (
-											child: new Text(
-												"Welcome, <firstname>!",
-												style: TextStyle(
-													fontWeight: FontWeight.bold,
-													fontSize: 30.0,
-													color: Colors.white,
-												)
-											)
-										)
-									)
-								)
-							),
 							// bike sample card 
 							BikeProfile("MyBitchinBike"),
 							// bike sample card 
-							BikeProfile("🅱️ike")
+							BikeProfile("🅱️ike"),
+							// bike sample card 
+							BikeProfile("MyBitchinBike"),
+							// bike sample card 
+							BikeProfile("🅱️ike"),
+							// bike sample card 
+							BikeProfile("MyBitchinBike"),
+							// bike sample card 
+							BikeProfile("🅱️ike"),
+							BikeProfile("MyBitchinBike"),
+							// bike sample card 
+							BikeProfile("🅱️ike"),
 						]
 					)
 				]
