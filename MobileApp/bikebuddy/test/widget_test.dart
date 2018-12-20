31355459def1d885396b8ed9b3961231269d02cb
Matthew Strong <mast4878@colorado.edu>
2018-12-19 19:30:27 -0700

    test

--- MobileApp/bikebuddy/test/widget_test.dart
+++ MobileApp/bikebuddy/test/widget_test.dart
@@ -1,12 +1,28 @@
 // This is a basic Flutter widget test.
+// using it for bike buddy
 // To perform an interaction with a widget in your test, use the WidgetTester utility that Flutter
 // provides. For example, you can send tap and scroll gestures. You can also use WidgetTester to
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
+//User	Acceptance	Test	
+//Plans
+//Create	a	document	that	describes	how	at	least	three features within	your	
+//finished	product	will	be	tested.		The	test	plan	should	include	specific	test	
+// (user	acceptance	test	cases) that	describe	the	data	and	the	user	
+//activity	that	will	be	executed	in	order	to	verify	proper	functionality	of	the	
+//feature.
+//Automated	Test	Cases Provide	a	link	to	the	tool	you	use	to	automate	testing,	or	explain	how	to	run	
+//the	automated	test	cases	or	schedule	time	with	the	TAs	to	demonstrate	your	
+//automated	tests.	 Provide	a	copy	of	the	output	showing	the	results	of	the	
+//automated	test	cases	running.	
+
+
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+//import 'package:test/test.dart';
 import 'package:bikebuddy/main.dart';
 
 void main() {
